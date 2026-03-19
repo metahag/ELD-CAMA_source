9e3908808732b5425f10b2b270d5ed0818778af4
André Kalmendal <andre.kalmendal@lnu.se>
2026-03-19 09:25:42 +0100

    Delete cama_frontend directory
    updating to public version

--- cama_frontend/src/api/apiConfig.ts
+++
@@ -1,8 +0,0 @@
-// Use the backend URL from environment variables
-export const apiUrl = import.meta.env.VITE_BACKEND_URL;
-
-//  API URL for local development
-//export const apiUrl = 'http://localhost:8000';
-//export const apiUrl = 'http://127.0.0.1:8000';
-
-
